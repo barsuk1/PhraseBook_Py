4490b69b7efac7c32897d679a32f255900545499
Igor Polevoy <igor.polevoy@gmail.com>
2017-10-05 00:06:15 -0700

    Get Google Translate PhraseBook from your Google account

--- google.py
+++ google.py
@@ -5,49 +5,7 @@ from html.parser import HTMLParser
 from html.entities import name2codepoint
 import lxml.html
 import getpass
-'''
-my ($self) = @_;
-    my $r = $self->_get("https://accgle.comounts.goo/ServiceLogin");
-    $r->content =~ m/GALX.*?value=\"(.*?)\"\>/gms ;
-    $self->{galx} = $1;
 
-    $r = $self->_post("https://accounts.google.com/ServiceLoginAuth",
-        [
-            GALX             => $self->{galx},
-            Email            => $self->{login},
-            Passwd           => $self->{pass},
-            PersistentCookie => 'yes',
-            bgresponse       => 'js_disabled',
-            continue         => 'https://translate.google.com'
-        ]
-    );
-
-    $r->content =~ /href=\"(.*)\"/gmsi;
-    my $redirect = $1;
-    # confirm login
-    $self->_get($redirect);
-    return $self;
-}
-sub get_key {
-    my ($self) = @_;
-    $self->{browser}->agent('Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36');
-    my $r = $self->_get('https://translate.google.com');
-    $r->content =~ /USAGE\='(.+?)'/gmsi;
-    $self->{google_key} = $1;
-    return $self;
-}
-
-# phrasebook as json blob
-sub get_pb {
-    my ($self) = @_;
-    my $r = $self->_post('https://translate.google.com/translate_a/sg?client=t&cm=g&hl=en&xt=' . $self->{google_key})->content;
-    $r =~ s/,,/,"",/gms; # json fix?
-    return decode_json($r);
-}
-
-
-
-'''
 
 def test_login(user, passwd):
     url = "https://accounts.google.com/ServiceLogin"
@@ -92,8 +50,7 @@ def test_login(user, passwd):
 
 
 if __name__ == '__main__':
-    #user = input("Please enter User Email: ")
-    #pwd = getpass.getpass("Please enter Password: ")
-    user="igor.polevoy@gmail.com"
-    pwd="baklan@334"
+    user = input("Please enter User Email: ")
+    pwd = getpass.getpass("Please enter Password: ")
+
     test_login(user , pwd)
